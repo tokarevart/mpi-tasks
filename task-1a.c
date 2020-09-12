f5a30ef70e135f31458d6d94b677d29e64c2fb29
pstu_mmsp_student 18 <pstu_mmsp_student_18@triton.icmm.ru>
2020-09-12 14:49:33 +0500

    init

---
+++ task-1a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank > 0) {
+        srand(rank);
+        int rnd = rand() % 101;
+        MPI_Send(&rnd, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("proc %d sent %d to %d\n", rank, rnd, 0);
+    } else {
+        for (int i = 1; i < size; ++i) {
+            int rnd;
+            MPI_Status status;
+            MPI_Recv(&rnd, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("proc %d received %d from %d\n", rank, rnd, i);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ task-1b.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <mpi.h>
+
+int sum_ints(int* nums, int size) {
+    int acc = 0;
+    for (int i = 0; i < size; ++i) {
+        acc += nums[i];
+    }
+    return acc;
+}
+
+int main(int argc, char** argv) {
+    int n = 50;
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank > 0) {
+        srand(rank);
+        int* nums = calloc(n, sizeof(int));
+        for (int i = 0; i < n; ++i) {
+            nums[i] = rand() % 100;
+        }
+        
+        MPI_Send(nums, n, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("proc %d sent nums with sum=%d to %d\n", rank, sum_ints(nums, n), 0);
+
+    } else {
+        int maxsum = -1;
+        int maxsum_rank = -1;
+
+        int* nums = calloc(n, sizeof(int));
+        for (int i = 1; i < size; ++i) {
+            MPI_Status status;
+            MPI_Recv(nums, n, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+            int sum = sum_ints(nums, n);
+            if (sum > maxsum) {
+                maxsum = sum;
+                maxsum_rank = i;
+            }
+
+            printf("proc %d received nums with sum=%d from %d\n", rank, sum, i);
+        }
+        free(nums);
+
+        printf("nums with max sum=%d received from %d\n", maxsum, maxsum_rank);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
