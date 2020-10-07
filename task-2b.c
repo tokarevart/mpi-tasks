c60816b23f8c5e56ba83b51324836c4f9cec9962
pstu_mmsp_student 18 <pstu_mmsp_student_18@triton.icmm.ru>
2020-10-08 02:39:01 +0500

    upd

--- task-2b.c
+++ task-2b.c
@@ -11,18 +11,6 @@ int min_int(int left, int right) {
     }
 }
 
-int to_odd(int idx) {
-    return idx * 2 + 1;
-}
-
-int to_even(int idx) {
-    return idx * 2;
-}
-
-int to_idx(int odd) {
-    return odd / 2;
-}
-
 bool is_odd(int idx) {
     return idx % 2 == 1;
 }
@@ -30,8 +18,6 @@ bool is_odd(int idx) {
 bool is_even(int idx) {
     return idx % 2 == 0;
 }
-
-typedef int (*IdxToRank)(int);
 
 typedef enum {
     FORWARD,
@@ -50,9 +36,67 @@ int circular_dec(int idx, int size) {
     }
 }
 
-void run_task_with_mapper(IdxToRank torank, Direction dir, int comm_rank, int comm_size, int n) {
-    if (torank(to_idx(comm_rank)) != comm_rank) {
-        printf("run_task_with_mapper fn got wrong params\n");
+typedef bool (*Filter)(int);
+
+int circular_inc_with_filter(int idx, int size, Filter filter) {
+    for (int i = 0; i < size; ++i) {
+        idx = circular_inc(idx, size);
+        if (filter(idx)) {
+            return idx;
+        }
+    }
+    return -1;
+}
+
+int circular_dec_with_filter(int idx, int size, Filter filter) {
+    for (int i = 0; i < size; ++i) {
+        idx = circular_dec(idx, size);
+        if (filter(idx)) {
+            return idx;
+        }
+    }
+    return -1;
+}
+
+int pos_in_filtered_seq(int idx, int size, Filter filter) {
+    int pos = 0;
+    for (int i = 0; i < size; ++i) {
+        if (!filter(i)) {
+            continue;
+        }
+        if (i == idx) {
+            return pos;
+        }
+        ++pos;
+    }
+    return -1;
+}
+
+int filtered_seq_size(int size, Filter filter) {
+    int first = -1;
+    for (int i = 0; i < size; ++i) {
+        if (filter(i)) {
+            first = i;
+            break;
+        }
+    }
+    if (first == -1) {
+        return 0;
+    }
+
+    int cur = first;
+    for (int i = 0; i < size; ++i) {
+        cur = circular_inc(cur, size);
+        if (filter(cur) && cur == first) {
+            return i + 1;
+        }
+    }
+    return -1;
+}
+
+void run_task_with_rank_filter(Filter filter, Direction dir, int comm_rank, int comm_size, int n) {
+    if (!filter(comm_rank)) {
+        printf("run_task_with_rank_filter fn got wrong comm_rank\n");
         return;
     }
 
@@ -62,23 +106,21 @@ void run_task_with_mapper(IdxToRank torank, Direction dir, int comm_rank, int co
     } else {
         dirmark = 'R';
     }
-    comm_rank = to_idx(comm_rank);
-    // yeah... it is magic, but i couldn't find a better way to compute the new comm_size
-    comm_size = to_idx(comm_size * 2 - min_int(torank(to_idx(comm_size)), comm_size));
 
     int prev_comm_rank;
     int next_comm_rank;
     if (dir == FORWARD) {
-        prev_comm_rank = circular_dec(comm_rank, comm_size);
-        next_comm_rank = circular_inc(comm_rank, comm_size);
+        prev_comm_rank = circular_dec_with_filter(comm_rank, comm_size, filter);
+        next_comm_rank = circular_inc_with_filter(comm_rank, comm_size, filter);
     } else {
-        prev_comm_rank = circular_inc(comm_rank, comm_size);
-        next_comm_rank = circular_dec(comm_rank, comm_size);
+        prev_comm_rank = circular_inc_with_filter(comm_rank, comm_size, filter);
+        next_comm_rank = circular_dec_with_filter(comm_rank, comm_size, filter);
     }
-    
+
+    int seq_size = filtered_seq_size(comm_size, filter);
     int acc = 0;
     for (int i = 0;; ++i) {
-        int count = i * comm_size + comm_rank;
+        int count = i * seq_size + pos_in_filtered_seq(comm_rank, comm_size, filter);
         if (count >= n) {
             break;
         }
@@ -86,23 +128,23 @@ void run_task_with_mapper(IdxToRank torank, Direction dir, int comm_rank, int co
         MPI_Request req;
         MPI_Status status;
         if (count > 0) {
-            MPI_Irecv(&acc, 1, MPI_INT, torank(prev_comm_rank), MPI_ANY_TAG, MPI_COMM_WORLD, &req);
+            MPI_Irecv(&acc, 1, MPI_INT, prev_comm_rank, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
             MPI_Wait(&req, &status);
-            printf("[%c] proc %d received acc=%d from %d\n", dirmark, torank(comm_rank), acc, torank(prev_comm_rank));
+            printf("[%c] proc %d received acc=%d from %d\n", dirmark, comm_rank, acc, prev_comm_rank);
         }
 
         int rnd = rand() % 101;
         acc += rnd;
-        printf("[%c] proc %d added %d to acc=%d\n", dirmark, torank(comm_rank), rnd, acc - rnd);
+        printf("[%c] proc %d added %d to acc=%d\n", dirmark, comm_rank, rnd, acc - rnd);
 
         if (count == n - 1) {
             printf("[%c] sum=%d\n", dirmark, acc);
             break;
         }
 
-        MPI_Isend(&acc, 1, MPI_INT, torank(next_comm_rank), 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(&acc, 1, MPI_INT, next_comm_rank, 0, MPI_COMM_WORLD, &req);
         MPI_Wait(&req, &status);
-        printf("[%c] proc %d sent acc=%d to %d\n", dirmark, torank(comm_rank), acc, torank(next_comm_rank));
+        printf("[%c] proc %d sent acc=%d to %d\n", dirmark, comm_rank, acc, next_comm_rank);
     }
 }
 
@@ -121,12 +163,12 @@ int main(int argc, char** argv) {
 
     if (is_even(comm_rank)) {
         MPI_Barrier(MPI_COMM_WORLD);
-        run_task_with_mapper(&to_even, FORWARD, comm_rank, comm_size, n);
+        run_task_with_rank_filter(&is_even, FORWARD, comm_rank, comm_size, n);
     }
 
     if (is_odd(comm_rank)) {
         MPI_Barrier(MPI_COMM_WORLD);
-        run_task_with_mapper(&to_odd, REVERSE, comm_rank, comm_size, n);
+        run_task_with_rank_filter(&is_odd, REVERSE, comm_rank, comm_size, n);
     }
 
     MPI_Finalize();
