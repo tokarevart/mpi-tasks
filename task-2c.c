4ce7a571897d079855516cfc4b4291e246ebcae8
pstu_mmsp_student 18 <pstu_mmsp_student_18@triton.icmm.ru>
2020-10-20 22:54:56 +0500

    upd

--- task-2c.c
+++ task-2c.c
@@ -10,6 +10,11 @@ typedef struct {
 } Message;
 
 void run_task(int comm_rank, int comm_size, int m, int n) {
+    bool wait_for_chain_final_sends = false;
+    bool wait_for_send = false;
+    MPI_Request* reqs = calloc(comm_size, sizeof(MPI_Request));
+    MPI_Request* sendrecv_reqs = reqs + comm_size - 2;
+
     if (comm_rank < m) {
         int rnd = rand() % 101;
         if (n == 1) {
@@ -22,9 +27,8 @@ void run_task(int comm_rank, int comm_size, int m, int n) {
         while (send_to == comm_rank) {
             send_to = rand() % comm_size;
         }
-        MPI_Request req;
-        MPI_Isend(&msg, sizeof(Message), MPI_BYTE, send_to, 0, MPI_COMM_WORLD, &req);
-        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        MPI_Issend(&msg, sizeof(Message), MPI_BYTE, send_to, 0, MPI_COMM_WORLD, sendrecv_reqs);
+        wait_for_send = true;
         printf(
             "proc %d sent     Message{chain_id=%d, acc_count=%d, acc=%d} to %d\n", 
             comm_rank, msg.chain_id, msg.acc_count, msg.acc, send_to
@@ -34,15 +38,22 @@ void run_task(int comm_rank, int comm_size, int m, int n) {
     int fin_chains_count = 0;
     while (fin_chains_count < m) {
         MPI_Request req;
-        MPI_Status status;
+        MPI_Status stats[2];
 
         Message msg;
-        MPI_Irecv(&msg, sizeof(Message), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
-        MPI_Wait(&req, &status);
-        printf(
-            "proc %d recieved Message{chain_id=%d, acc_count=%d, acc=%d} from %d\n", 
-            comm_rank, msg.chain_id, msg.acc_count, msg.acc, status.MPI_SOURCE
-        );
+        MPI_Irecv(&msg, sizeof(Message), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, sendrecv_reqs + 1);
+        if (wait_for_chain_final_sends) {
+            MPI_Waitall(comm_size, reqs, MPI_STATUSES_IGNORE);
+            wait_for_chain_final_sends = false;
+        } else if (wait_for_send) {
+            MPI_Waitall(2, sendrecv_reqs, stats);
+            wait_for_send = false;
+            printf(
+                "proc %d recieved Message{chain_id=%d, acc_count=%d, acc=%d} from %d\n", 
+                comm_rank, msg.chain_id, msg.acc_count, msg.acc, stats[1].MPI_SOURCE
+            );
+        } 
+        
         if (msg.acc_count == n) {
             ++fin_chains_count;
             continue;
@@ -59,15 +70,18 @@ void run_task(int comm_rank, int comm_size, int m, int n) {
             ++fin_chains_count;
             printf("chain %d sum=%d\n", msg.chain_id, msg.acc);
 
-            MPI_Request* reqs = calloc(comm_size - 1, sizeof(MPI_Request));
             for (int i = 0; i < comm_rank; ++i) {
-                MPI_Isend(&msg, sizeof(Message), MPI_BYTE, i, 0, MPI_COMM_WORLD, reqs + i);
+                MPI_Issend(&msg, sizeof(Message), MPI_BYTE, i, 0, MPI_COMM_WORLD, reqs + i);
             }
             for (int i = comm_rank + 1; i < comm_size; ++i) {
-                MPI_Isend(&msg, sizeof(Message), MPI_BYTE, i, 0, MPI_COMM_WORLD, reqs + i - 1);
+                MPI_Issend(&msg, sizeof(Message), MPI_BYTE, i, 0, MPI_COMM_WORLD, reqs + i - 1);
             }
-            MPI_Waitall(comm_size - 1, reqs, MPI_STATUSES_IGNORE);
-            free(reqs);
+
+            if (fin_chains_count == m) {
+                MPI_Waitall(comm_size - 1, reqs, MPI_STATUSES_IGNORE);
+                break;
+            }
+            wait_for_chain_final_sends = true;
             continue;
         }
 
@@ -75,18 +89,20 @@ void run_task(int comm_rank, int comm_size, int m, int n) {
         while (send_to == comm_rank) {
             send_to = rand() % comm_size;
         }
-        MPI_Isend(&msg, sizeof(Message), MPI_BYTE, send_to, 0, MPI_COMM_WORLD, &req);
-        MPI_Wait(&req, MPI_STATUS_IGNORE);
+        MPI_Issend(&msg, sizeof(Message), MPI_BYTE, send_to, 0, MPI_COMM_WORLD, sendrecv_reqs);
+        wait_for_send = true;
         printf(
             "proc %d sent     Message{chain_id=%d, acc_count=%d, acc=%d} to %d\n", 
             comm_rank, msg.chain_id, msg.acc_count, msg.acc, send_to
         );
     }
+
+    free(reqs);
 }
 
 int main(int argc, char** argv) {
     int m = 2;
-    int n = 5;
+    int n = 2;
 
     int comm_rank, comm_size;
     MPI_Init(&argc, &argv);
--- task-2c.sh
+++ task-2c.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mpicc task-2c.c -o task-2c -lm -O3 -std=c11 &&
-sbatch -n 3 -t 1 -p debug --wrap "mpiexec ./task-2c"
+sbatch -n 2 -t 1 -p debug --wrap "mpiexec ./task-2c"
